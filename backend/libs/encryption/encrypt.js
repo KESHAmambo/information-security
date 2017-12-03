cb8a7dbfaeabaa91119d76532874817700ec9b12
Arkady Baranok <arkadii.baranok@netcracker.com>
2017-12-03 20:16:21 +0300

    Encryption node module

--- backend/libs/encryption/encrypt.js
+++ backend/libs/encryption/encrypt.js
@@ -5,3 +5,20 @@ var text = SecretSharing.solve(dictionary.shares, 7, 16);
 
 console.log('Text: ', text);
 
+var Encryptor = function() {};
+
+Encryptor.encrypt = function(text, users) {
+    var result = {};
+
+    return result;
+};
+
+Encryptor.decrypt = function(encryptedText) {
+    var text = '';
+
+    return text;
+};
+
+var encryptor = new Encryptor();
+
+module.exports = encryptor;
